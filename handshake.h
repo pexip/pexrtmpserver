575efe0f251d9e0d324580b8a041220087531f1f
Havard Graff <havard.graff@gmail.com>
2014-09-04 01:19:03 +0200

    rtmpserver: integrate new handshake into the server
    Exciting to see if Flash likes it! :)

--- handshake.h
+++ handshake.h
@@ -3,14 +3,19 @@
 
 #include <gst/gst.h>
 
+#define HANDSHAKE_LENGTH 1536
+
 typedef struct _PexRtmpHandshake PexRtmpHandshake;
 
 PexRtmpHandshake * pex_rtmp_handshake_new ();
 void pex_rtmp_handshake_free (PexRtmpHandshake * hs);
-void pex_rtmp_handshake_process (PexRtmpHandshake * hs,
+gboolean pex_rtmp_handshake_process (PexRtmpHandshake * hs,
     const guint8 * data, gint len);
 
 guint8 * pex_rtmp_handshake_get_buffer (PexRtmpHandshake * hs);
 guint pex_rtmp_handshake_get_length (PexRtmpHandshake * hs);
+
+gboolean pex_rtmp_handshake_verify_reply (PexRtmpHandshake * hs,
+    guint8 reply[HANDSHAKE_LENGTH]);
 
 #endif /* __PEX_RTMP_HANDSHAKE_H__ */
