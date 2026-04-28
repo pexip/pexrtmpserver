4083225c70fc2c1f887a104f7be374dbe15e6616
Huw Jones <huw@pexip.com>
2026-04-28 11:22:04 +0100

    pexrtmpserver: allow listening on port 0

--- src/pexrtmpserver.c
+++ src/pexrtmpserver.c
@@ -743,11 +743,11 @@ rtmp_server_do_poll (PexRtmpServer * srv)
   }
 
   /* check for new connections */
-  if (srv->port && gst_poll_fd_can_read (srv->fd_set, &srv->listen_gfd)) {
+  if (srv->port >= 0 && gst_poll_fd_can_read (srv->fd_set, &srv->listen_gfd)) {
     rtmp_server_create_client (srv, srv->listen_gfd.fd);
     return TRUE;
   }
-  if (srv->ssl_port && gst_poll_fd_can_read (srv->fd_set, &srv->listen_ssl_gfd)) {
+  if (srv->ssl_port >= 0 && gst_poll_fd_can_read (srv->fd_set, &srv->listen_ssl_gfd)) {
     rtmp_server_create_client (srv, srv->listen_ssl_gfd.fd);
     return TRUE;
   }
@@ -860,7 +860,7 @@ gboolean
 pex_rtmp_server_start (PexRtmpServer * srv)
 {
   /* listen for normal and ssl connections */
-  if (srv->port) {
+  if (srv->port >= 0) {
     srv->listen_fd = tcp_listen (srv->port);
     if (srv->listen_fd <= 0) {
       GST_ERROR_OBJECT (srv, "Could not listen on port %d", srv->port);
@@ -872,7 +872,7 @@ pex_rtmp_server_start (PexRtmpServer * srv)
     gst_poll_fd_ctl_read (srv->fd_set, &srv->listen_gfd, TRUE);
   }
 
-  if (srv->ssl_port) {
+  if (srv->ssl_port >= 0) {
 #ifdef HAVE_OPENSSL
     srv->listen_ssl_fd = tcp_listen (srv->ssl_port);
     if (srv->listen_ssl_fd <= 0) {
@@ -1187,12 +1187,12 @@ pex_rtmp_server_class_init (PexRtmpServerClass * klass)
 
   g_object_class_install_property (gobject_class, PROP_PORT,
       g_param_spec_int ("port", "Port",
-          "The port to listen on", 0, 65535, DEFAULT_PORT,
+          "The port to listen on", -1, 65535, DEFAULT_PORT,
           G_PARAM_CONSTRUCT | G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
 
   g_object_class_install_property (gobject_class, PROP_SSL_PORT,
       g_param_spec_int ("ssl-port", "Port",
-          "The port to listen on", 0, 65535, DEFAULT_SSL_PORT,
+          "The port to listen on", -1, 65535, DEFAULT_SSL_PORT,
           G_PARAM_CONSTRUCT | G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
 
   g_object_class_install_property (gobject_class, PROP_CERT_FILE,
