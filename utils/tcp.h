af8c3b6119cbd767e3847db86cafaad42e08a7cc
Camilo Celis Guzman <camilo@pexip.com>
2026-04-29 22:23:41 +0900

    tcp: correct return type for get_listen_port

--- utils/tcp.h
+++ utils/tcp.h
@@ -51,6 +51,6 @@ void tcp_set_nonblock (gint fd, gboolean enabled);
 PEX_RTMPSERVER_EXPORT
 gboolean tcp_is_localhost (gint fd);
 PEX_RTMPSERVER_EXPORT
-gint tcp_get_listen_port (gint fd, gint* port);
+gboolean tcp_get_listen_port (gint fd, gint* port);
 
 #endif /* __TCP_H__ */
